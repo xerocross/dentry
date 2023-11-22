0a1146bbeccceed0120febb3099942ca66861682
Adam Cross <adamfgcross@gmail.com>
2023-11-22 16:05:51 -0500

    initial commit

---
+++ src/main/java/com/adamfgcross/dentry/DentryApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.dentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/dentry/DentryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.dentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
